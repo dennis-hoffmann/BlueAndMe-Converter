a8ca5523b88e8424fac33e535c421a1953209240
Dennis Hoffmann <49145762+dennis-hoffmann@users.noreply.github.com>
2022-09-08 18:26:09 +0200

    Fix and update stuff (#3)

--- app
+++ app
@@ -1,12 +1,14 @@
 #!/usr/bin/env php
 <?php
 
+ini_set("error_reporting", E_ALL & ~E_DEPRECATED);
+
 require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
-$application->add(new blueMe\ConvertCommand());
+$application->add(new App\ConvertCommand());
 
 $application->run();
--- src/ConvertCommand.php
+++ src/ConvertCommand.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace blueMe;
+namespace App;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,8 +18,6 @@ use Symfony\Component\Process\Process;
  * Requires FFmpeg https://wiki.ubuntuusers.de/FFmpeg/
  * and
  * mp3gain https://wiki.ubuntuusers.de/MP3Gain/
- *
- * @package App
  */
 class ConvertCommand extends Command
 {
@@ -37,57 +36,90 @@ class ConvertCommand extends Command
      */
     protected $tagger;
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->finder = new Finder();
+        $this->filesystem = new Filesystem();
+        $this->tagger = new \getID3();
+    }
+
     /**
      * Configure command.
      */
     protected function configure()
     {
-        $this->setName('convert')
+        $this
+            ->setName('convert')
             ->setDescription('Convert audio files for use in Fiat blue&me car radio systems.')
-            ->setHelp('You may specify either a source directory or a text file containing absolute paths to audio files');
+            ->setHelp('You may specify either a source directory or a text file containing absolute paths to audio files')
+        ;
 
-        $this->addOption(
+        $this->addArgument(
             'source',
-            's',
-            InputOption::VALUE_REQUIRED,
-            'Source directory or text file with paths. Set "-x" if using text file.');
+            InputArgument::REQUIRED,
+            'Source directory or .xspf file. Set "-x" if using .xspf file.'
+        );
+
+        $this->addArgument(
+            'target',
+            InputArgument::REQUIRED,
+            'Target directory where converted files will be written.'
+        );
 
         $this->addOption(
             'xspf',
             'x',
             InputOption::VALUE_NONE,
-            'Must be set when using a text file');
+            'Must be set when using a .xspf file'
+        );
 
         $this->addOption(
             'force',
             'f',
             InputOption::VALUE_NONE,
-            'Force convert even when already mp3');
-
-        $this->addOption(
-            'target-dir',
-            't',
-            InputOption::VALUE_REQUIRED,
-            'Target Directory Root.');
-
-        $this->finder     = new Finder();
-        $this->filesystem = new Filesystem();
-        $this->tagger     = new \getID3();
+            'Force convert even when already mp3'
+        );
     }
 
-    /**
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return int|void|null
-     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if ($input->getOption('xspf')) {
-            $file   = simplexml_load_file($input->getOption('source'));
-            $tracks = $this->parseXSPF($file);
+        $force = $input->hasOption('force');
+        $outputDirectory = $input->getArgument('target');
+
+        if (!is_dir($outputDirectory)) {
+            throw new \RuntimeException(sprintf('Output directory "%s" does not exist.'));
+        }
+
+        if ($playlistFile = $input->getOption('xspf')) {
+            $xml = simplexml_load_file($playlistFile);
+
+            if (!$xml) {
+                throw new \RuntimeException(sprintf('Unable to parse XML file "%s".', $playlistFile));
+            }
+
+            $tracks = $this->parseXSPF($xml);
         } else {
-            $tracks = $this->finder->ignoreUnreadableDirs()->in($input->getOption('source'))->name('*.flac')->name('*.m4a')->name('*.mp3')->name('*.wma')->name('*.aac')->files();
+            $inputDirectory = $input->getArgument('source');
+
+            if (!is_dir($inputDirectory)) {
+                throw new \RuntimeException(sprintf('Input directory "%s" does not exist.', $inputDirectory));
+            }
+
+            $tracks = $this
+                ->finder
+                ->ignoreUnreadableDirs()
+                ->in($inputDirectory)
+                ->name('*.flac')
+                ->name('*.oga')
+                ->name('*.m4a')
+                ->name('*.mp3')
+                ->name('*.wma')
+                ->name('*.aac')
+                ->name('*.wav')
+                ->files()
+            ;
         }
 
         if (empty($tracks)) {
@@ -95,10 +127,10 @@ class ConvertCommand extends Command
             exit;
         }
 
-        $output->writeln(sprintf('<info>Will work with %d files</info>', count($tracks)));
+        $output->writeln(sprintf('<info>Will work with %d files.</info>', count($tracks)));
 
         foreach ($tracks as $track) {
-            $this->convert($track, $input->getOption('target-dir'), $output, $input->hasOption('force'));
+            $this->convert($track, $outputDirectory, $output, $force);
 
             $output->writeln(sprintf('<info>Converted %s.</info>', $track->getFilename()));
         }
@@ -112,31 +144,28 @@ class ConvertCommand extends Command
      * strip all tags other than id3v1,
      * and normalize the audio volume.
      *
-     * @param \SplFileInfo    $file
-     * @param string          $target
+     * @param \SplFileInfo $file
+     * @param string $target
      * @param OutputInterface $output
-     * @param bool            $force
+     * @param bool $force
      *
      * @return bool
      */
     protected function convert(\SplFileInfo $file, string $target, OutputInterface $output, $force = false): bool
     {
-        $filename = basename($file->getFilename(), $file->getExtension());
+        $filename = pathinfo($file->getFilename(), PATHINFO_FILENAME);
+
         $fileInfo = $this->tagger->analyze($file->getPathname());
-
         \getid3_lib::CopyTagsToComments($fileInfo);
-        $tags = array_map(function ($element) {
-            return $element[0];
-        }, $fileInfo['comments_html'] ?? []);
-
+        $tags = array_column($fileInfo['comments_html'] ?? [], 0);
 
         if (!empty($tags)) {
-            $title  = $tags['title'] ?? '';
-            $album  = $tags['album'] ?? '';
+            $title = $tags['title'] ?? '';
+            $album = $tags['album'] ?? '';
             $artist = $tags['albumartist'] ?? '';
 
             if ($artist === '') {
-                $artist              = $tags['artist'] ?? '';
+                $artist = $tags['artist'] ?? '';
                 $tags['albumartist'] = $artist;
             }
 
@@ -150,15 +179,17 @@ class ConvertCommand extends Command
         }
 
         $filename = self::normalizeString($filename);
-        $outputFile = $target . '/' . $filename . '.mp3';
+        $outputFile = $target . DIRECTORY_SEPARATOR . $filename . '.mp3';
 
         $this->filesystem->mkdir($target, 0775);
 
         if ($force || $file->getExtension() !== 'mp3') {
             $command = sprintf('ffmpeg -i "%s" -ab 320k "%s"', $file->getPathname(), $outputFile);
-            $process = new Process($command);
-            // Set timeout to one minute
-            $process->setTimeout(60);
+
+            $process = Process::fromShellCommandline($command)
+                ->setTimeout(160)
+            ;
+
             $process->start();
 
             foreach ($process as $type => $data) {
@@ -192,18 +223,18 @@ class ConvertCommand extends Command
      * Write id3v1 tags to file and strip other tags.
      *
      * @param string $fileName
-     * @param array  $tags
+     * @param array $tags
      *
      * @return bool
      */
     private function writeTags(string $fileName, array $tags = []): bool
     {
         $tagData = [
-            'TITLE'       => [''],
-            'ARTIST'      => [''],
-            'ALBUM'       => [''],
-            'YEAR'        => [''],
-            'COMMENT'     => [''],
+            'TITLE' => [''],
+            'ARTIST' => [''],
+            'ALBUM' => [''],
+            'YEAR' => [''],
+            'COMMENT' => [''],
             'TRACKNUMBER' => [''],
         ];
 
@@ -231,13 +262,13 @@ class ConvertCommand extends Command
             $tagData['TRACKNUMBER'][0] = self::normalizeString($tags['track']);
         }
 
-        $tagWriter                    = new \getid3_writetags;
-        $tagWriter->filename          = $fileName;
-        $tagWriter->tagformats        = ['id3v1'];
-        $tagWriter->overwrite_tags    = true;
-        $tagWriter->tag_encoding      = 'ISO-8859';
+        $tagWriter = new \getid3_writetags;
+        $tagWriter->filename = $fileName;
+        $tagWriter->tagformats = ['id3v1'];
+        $tagWriter->overwrite_tags = true;
+        $tagWriter->tag_encoding = 'ISO-8859-1';
         $tagWriter->remove_other_tags = true;
-        $tagWriter->tag_data          = $tagData;
+        $tagWriter->tag_data = $tagData;
 
         if ($tagWriter->WriteTags()) {
             return true;
@@ -249,7 +280,7 @@ class ConvertCommand extends Command
     /**
      * Normalize audio track using mp3gain.
      *
-     * @param string          $targetFile
+     * @param string $targetFile
      * @param OutputInterface $output
      *
      * @return bool
@@ -257,9 +288,8 @@ class ConvertCommand extends Command
     private function normalize(string $targetFile, OutputInterface $output): bool
     {
         $command = sprintf('mp3gain -p -r -c -s s "%s"', $targetFile);
-        $process = new Process($command);
-        // Set timeout to one minute
-        $process->setTimeout(60);
+        $process = Process::fromShellCommandline($command)->setTimeout(60);
+
         $process->start();
 
         foreach ($process as $type => $data) {
@@ -276,7 +306,7 @@ class ConvertCommand extends Command
      *
      * @return array
      */
-    private function parseXSPF(\SimpleXMLElement $xml)
+    private function parseXSPF(\SimpleXMLElement $xml): array
     {
         $tracks = [];
 
